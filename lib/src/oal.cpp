a4439774f9a985129e73d89b9ca14cc3e04c3282
tapxyh4ik <dadkad228@gmail.com>
2024-12-28 20:17:59 +0300

    Update oal.cpp

--- lib/src/oal.cpp
+++ lib/src/oal.cpp
@@ -4,27 +4,27 @@
 bool oal::check_al_errors(const std::string& filename, const std::uint_fast32_t line) {
 	ALenum error = alGetError();
 	if (error != AL_NO_ERROR) {
-		std::cerr << "AuralDynamics : [***ERROR***](" << filename << ":" << line << ")" << std::endl;
+		std::cerr << "AuralDynamics : [***ERROR***](" << filename << ":" << line << ")";
 
 		switch (error)
 		{
 		case AL_INVALID_NAME:
-			std::cerr << "AL_INVALID_NAME: a bad name (ID) was passed to an OpenAL function" << std::endl;
+			std::cerr << "AL_INVALID_NAME: a bad name (ID) was passed to an OpenAL function";
 			break;
 		case AL_INVALID_ENUM:
-			std::cerr << "AL_INVALID_ENUM: an invalid enum value was passed to an OpenAL function" << std::endl;
+			std::cerr << "AL_INVALID_ENUM: an invalid enum value was passed to an OpenAL function";
 			break;
 		case AL_INVALID_VALUE:
-			std::cerr << "AL_INVALID_VALUE: an invalid value was passed to an OpenAL function" << std::endl;
+			std::cerr << "AL_INVALID_VALUE: an invalid value was passed to an OpenAL function";
 			break;
 		case AL_INVALID_OPERATION:
-			std::cerr << "AL_INVALID_OPERATION: the requested operation is not valid" << std::endl;
+			std::cerr << "AL_INVALID_OPERATION: the requested operation is not valid";
 			break;
 		case AL_OUT_OF_MEMORY:
-			std::cerr << "AL_OUT_OF_MEMORY: the requested operation resulted in OpenAL running out of memory" << std::endl;
+			std::cerr << "AL_OUT_OF_MEMORY: the requested operation resulted in OpenAL running out of memory";
 			break;
 		default:
-			std::cerr << "UNKNOWN AL ERROR: " << error << std::endl;
+			std::cerr << "UNKNOWN AL ERROR: " << error;
 		}
 
 		std::cerr << std::endl;
@@ -38,7 +38,7 @@ bool oal::check_alc_errors(const std::string& filename, const std::uint_fast32_t
 	ALCenum error = alcGetError(device);
 	if (error != ALC_NO_ERROR)
 	{
-		std::cerr << "***ERROR*** (" << filename << ": " << line << ")\n";
+		std::cerr << "AuralDynamics : [***ERROR***](" << filename << ": " << line << ")\n";
 		switch (error)
 		{
 		case ALC_INVALID_VALUE:
